a0f16f2c3cfd55c71f3520381cf34bddbd064d25
BPM <brantleymorris@me.com>
2020-10-26 13:30:44 -0500

    add variable assignment for character types

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// My code is below
+
+var passwordLength = prompt("How many characters would you like the password to include?");
+// need to parseInt prompt value
+
+if (passwordLength < 8 || passwordLength > 128) {
+  confirm("Your password cannot be less than 8 character or more than 128 characters.");
+  prompt("How many characters would you like the password to include?");
+}
+// unsure if this needs an else statement
+
+var lowercase = confirm("Would you like your password to include lowercase letter?");
+// add if for lowercase=false
+
+var uppercase = confirm("Would you like your password to include uppercase letters?");
+// add if for uppercase=false
+
+var numeric = confirm("Would you like your password to include numbers?");
+// add if for numberic=false
+
+var specialCharacters = confirm("Would you like your password to include speical characters");
+// add if for specialCharacters=false
