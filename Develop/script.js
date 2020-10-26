9070c57a029b2c69ed9cd3ff4d806e812343a1b6
BPM <brantleymorris@me.com>
2020-10-26 13:52:58 -0500

    added if else statements to character assignment

--- Develop/script.js
+++ Develop/script.js
@@ -19,19 +19,53 @@ var passwordLength = prompt("How many characters would you like the password to 
 // need to parseInt prompt value
 
 if (passwordLength < 8 || passwordLength > 128) {
-  confirm("Your password cannot be less than 8 character or more than 128 characters.");
-  prompt("How many characters would you like the password to include?");
+  alert("Your password cannot be less than 8 character or more than 128 characters.");
+  passwordLength = prompt("How many characters would you like the password to include?");
 }
 // unsure if this needs an else statement
 
 var lowercase = confirm("Would you like your password to include lowercase letter?");
 // add if for lowercase=false
+  if (lowercase === true) {
+    alert("Your password can include lowercase letter.");
+  }
+  else  { // unsure why this is error-> else doesn't need selector
+    alert("Your password will not include lowercase letters");
+  }
+  
 
 var uppercase = confirm("Would you like your password to include uppercase letters?");
 // add if for uppercase=false
+if (uppercase === true) {
+  alert("Your password can include uppercase letters.");
+}
+else {
+  alert("Your password will not include uppercase letters.");
+}
 
 var numeric = confirm("Would you like your password to include numbers?");
 // add if for numberic=false
+if (numeric === true) {
+  alert("Your password can include numeric values.");
+}
+else {
+  alert("Your password will not include numeric values.");
+}
 
 var specialCharacters = confirm("Would you like your password to include speical characters");
 // add if for specialCharacters=false
+if (specialCharacters === true) {
+  alert("Your password can include special characters.");
+}
+else {
+  alert("Your password will not include special characters.");
+}
+
+// may need final if statement for having all character types false
+if (lowercase === false && uppercase === false && numeric === false && specialCharacters === false) {
+  alert("Your password must include at lease one of the following: lowercase letter, uppercase letters, numeric values, or special characters.");
+  lowercase = confirm("Would you like your password to include lowercase letters?");
+  uppercase = confirm("Would you like your password to include uppercase letters?");
+  numeric = confirm("Would you like your password to include numbers?");
+  specialCharacters = confirm("Would you like your password to include speical characters");
+}
